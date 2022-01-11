e00e1c7eea6a568c88fe81ad6e7b15aa3f8193bf
Pavel Buzin <84155505+PaulBuzz@users.noreply.github.com>
2022-01-11 16:15:13 +0200

    Update sql_hw_2.sql

--- sql_hw_2.sql
+++ sql_hw_2.sql
@@ -1,10 +1,10 @@
-1. Создать таблицу employees.
+--1. Создать таблицу employees.
 create table employees(
 	id serial primary key,
 	employee_name varchar(50) not null
 );
 
-2. Наполнить таблицу employee 70 строками.
+--2. Наполнить таблицу employee 70 строками.
 insert into employees(employee_name)
 values ('Pavel Buzin'),
 	   ('Alina Volovich'),
@@ -78,13 +78,13 @@ values ('Pavel Buzin'),
 	   ('Mark Lobov'),
 	   ('Semen Grinayev');
 	   
-3. Создать таблицу salary.
+--3. Создать таблицу salary.
 create table Salary(
 	id serial primary key,
 	monthly_salary int not null
 );
 
-4. Наполнить таблицу salary 15 строками:
+--4. Наполнить таблицу salary 15 строками:
 insert into Salary(monthly_salary)
 values (1000),
 	   (1100),
@@ -103,15 +103,15 @@ values (1000),
 	   (2400),
 	   (2500);
 	   
-5. Создать таблицу employee_salary
+--5. Создать таблицу employee_salary
 create table employee_salary(
 	id serial primary key,
 	employee_id int unique not null,
 	salary_id int not null
 );
 
-6. Наполнить таблицу employee_salary 40 строками:
-6.1. в 10 строк из 40 вставить несуществующие employee_id
+--6. Наполнить таблицу employee_salary 40 строками:
+--в 10 строк из 40 вставить несуществующие employee_id
 insert into employee_salary(employee_id,salary_id)
 values (3, 7),
 	   (1, 4),
@@ -154,19 +154,19 @@ values (3, 7),
 	   (62, 12),
 	   (66, 13);
 	  
-7. Создать таблицу roles.
-7.1. id. Serial  primary key,
-7.2. role_name. int, not null, unique
+--7. Создать таблицу roles.
+--id. Serial  primary key,
+--role_name. int, not null, unique
 create table roles(
 	id serial primary key,
 	role_name int unique not null
 );
 
-8. Поменять тип столба role_name с int на varchar(30)
+--8. Поменять тип столба role_name с int на varchar(30)
 alter table roles
 alter column role_name type varchar(40) using role_name::varchar(30);
 
-9. Наполнить таблицу roles 20 строками:
+--9. Наполнить таблицу roles 20 строками:
 insert into roles(role_name)
 values ('Junior Python developer'),
 	   ('Middle Python developer'),
@@ -189,10 +189,10 @@ values ('Junior Python developer'),
 	   ('Middle Automation QA engineer'),
 	   ('Senior Automation QA engineer');
 	  
-10. Создать таблицу roles_employee
-10.1. id. Serial  primary key,
-10.2. employee_id. Int, not null, unique (внешний ключ для таблицы employees, поле id)
-10.3. role_id. Int, not null (внешний ключ для таблицы roles, поле id)
+--10. Создать таблицу roles_employee
+--id. Serial  primary key,
+--employee_id. Int, not null, unique (внешний ключ для таблицы employees, поле id)
+--role_id. Int, not null (внешний ключ для таблицы roles, поле id)
 create table roles_employee(
 	id serial primary key,
 	employee_id int unique not null,
@@ -203,7 +203,7 @@ create table roles_employee(
 		references roles(id)
 );	  
 	  
-11. Наполнить таблицу roles_employee 40 строками:
+--11. Наполнить таблицу roles_employee 40 строками:
 insert into roles_employee(employee_id, role_id)
 values (7, 2),
  	   (20, 4),
